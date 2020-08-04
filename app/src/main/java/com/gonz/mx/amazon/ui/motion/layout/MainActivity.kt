428962fcae898dd53ba4e63b3abda9a0567f146d
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-03 20:11:22 -0500

    Cerate first letter animation.

--- app/src/main/java/com/gonz/mx/amazon/ui/motion/layout/MainActivity.kt
+++ app/src/main/java/com/gonz/mx/amazon/ui/motion/layout/MainActivity.kt
@@ -2,15 +2,16 @@ package com.gonz.mx.amazon.ui.motion.layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gonz.mx.amazon.ui.motion.layout.databinding.ActivityLettersBinding
 import com.gonz.mx.amazon.ui.motion.layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLettersBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+        binding = ActivityLettersBinding.inflate(layoutInflater).apply {
             setContentView(root)
         }
     }
